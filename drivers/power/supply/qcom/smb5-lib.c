e8831758e4eb53c5347ffd44be231da42b7021d3
Dyneteve <dyneteve@pixelexperience.org>
2020-06-02 17:03:52 +0200

    drivers: power: supply: qcom: Drop the batt_psy checks and use 100mA to reset input

--- drivers/power/supply/qcom/smb5-lib.c
+++ drivers/power/supply/qcom/smb5-lib.c
@@ -7583,8 +7583,7 @@ irqreturn_t wdog_snarl_irq_handler(int irq, void *data)
 		schedule_delayed_work(&chg->thermal_regulation_work, 0);
 	}
 
-	if (chg->step_chg_enabled)
-		power_supply_changed(chg->batt_psy);
+	power_supply_changed(chg->batt_psy);
 
 	return IRQ_HANDLED;
 }
@@ -7930,8 +7929,7 @@ static void bms_update_work(struct work_struct *work)
 
 	smblib_suspend_on_debug_battery(chg);
 
-	if (chg->batt_psy)
-		power_supply_changed(chg->batt_psy);
+	power_supply_changed(chg->batt_psy);
 }
 
 static void pl_update_work(struct work_struct *work)
