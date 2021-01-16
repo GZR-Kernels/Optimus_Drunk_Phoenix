797fe409bb083e52411215b94d8ad3606ac9833b
Yaroslav Furman <yaro330@gmail.com>
2021-01-16 13:54:44 +0300

    sched/tune: only force UX tasks to big cores
    If we force top-app's boost value to be >0 we get excellent
    jitter values due to top-app being rendered on big cores.
    
    However along with the actual top app a load of crap ends up on
    big cluster too. We can avoid this by checking if adj_score is 0,
    which means that this app is actually the one that's currently on
    the screen (or it's one of its child processes).
    
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -546,6 +546,22 @@ int schedtune_cpu_boost(int cpu)
 	return bg->boost_max;
 }
 
+static inline bool schedtune_adj_ta(struct schedtune *st, struct task_struct *p)
+{
+	char name_buf[NAME_MAX + 1];
+	int adj = p->signal->oom_score_adj;
+
+	cgroup_name(st->css.cgroup, name_buf, sizeof(name_buf));
+	if (!strncmp(name_buf, "top-app", strlen("top-app"))) {
+		if ((adj == 0) && !(p->flags & PF_KTHREAD)) {
+			pr_debug("top app is %s\n", p->comm);
+			return true;
+		}
+	}
+
+	return false;
+}
+
 int schedtune_task_boost(struct task_struct *p)
 {
 	struct schedtune *st;
@@ -557,8 +573,26 @@ int schedtune_task_boost(struct task_struct *p)
 	/* Get task boost value */
 	rcu_read_lock();
 	st = task_schedtune(p);
-	task_boost = st->boost;
+	task_boost = st->boost || schedtune_adj_ta(st, p);
 	rcu_read_unlock();
+
+	return task_boost;
+}
+
+/*  The same as schedtune_task_boost except assuming the caller has the rcu read
+ *  lock.
+ */
+int schedtune_task_boost_rcu_locked(struct task_struct *p)
+{
+	struct schedtune *st;
+	int task_boost;
+
+	if (unlikely(!schedtune_initialized))
+		return 0;
+
+	/* Get task boost value */
+	st = task_schedtune(p);
+	task_boost = st->boost || schedtune_adj_ta(st, p);
 
 	return task_boost;
 }
